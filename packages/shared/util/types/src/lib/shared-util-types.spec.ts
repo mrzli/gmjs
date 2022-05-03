0a39527a79c2a609d91c008ef9b67461e429203b
Goran Mržljak <goran.mrzljak@gmail.com>
2022-05-03 19:56:43 +0200

    add library 'types'

---
+++ packages/shared/util/types/src/lib/shared-util-types.spec.ts
@@ -0,0 +1,7 @@
+import { sharedUtilTypes } from './shared-util-types';
+
+describe('sharedUtilTypes', () => {
+  it('should work', () => {
+    expect(sharedUtilTypes()).toEqual('shared-util-types');
+  });
+});
