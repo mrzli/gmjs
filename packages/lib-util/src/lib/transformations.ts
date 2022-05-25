a5a708876e4e7f233077e0a07dfa19be60628649
Goran Mržljak <goran.mrzljak@gmail.com>
2022-05-25 19:56:52 +0200

    publish

--- packages/lib-util/src/lib/transformations.ts
+++ packages/lib-util/src/lib/transformations.ts
@@ -1,15 +1,13 @@
-import { JsonValue } from 'type-fest';
+import { AnyValue } from '@gmjs/util';
 
-export function textToJson<T extends JsonValue = JsonValue>(
-  content: string
-): T {
+export function textToJson<T extends AnyValue = AnyValue>(content: string): T {
   return JSON.parse(content);
 }
 
-export function jsonToText<T extends JsonValue = JsonValue>(json: T): string {
+export function jsonToText<T extends AnyValue = AnyValue>(json: T): string {
   return JSON.stringify(json);
 }
 
-export function jsonToPretty<T = JsonValue>(json: T): string {
+export function jsonToPretty<T = AnyValue>(json: T): string {
   return `${JSON.stringify(json, null, 2)}\n`;
 }
