6db8d407f0e7a8909d7e2a0a88d0bda11be64184
Goran Mržljak <goran.mrzljak@gmail.com>
2022-06-25 21:04:22 +0200

    implement mongo schema visit function

---
+++ packages/data-manipulation/src/lib/shared/mongo-schema-visit/mongo-schema-visit.spec.ts
@@ -0,0 +1,80 @@
+import {
+  MongoJsonSchemaPropertyContext,
+  MongoJsonSchemaVisitor,
+  mongoSchemaVisit,
+} from './mongo-schema-visit';
+import {
+  MongoJsonSchemaAnyType,
+  MongoJsonSchemaTypeObject,
+} from '../mongo-json-schema';
+import path from 'path';
+import {
+  createFileSystemExampleTest,
+  ExampleMappingFn,
+  getFileSystemTestExamples,
+} from '@gmjs/test-util';
+import { textToJson } from '@gmjs/lib-util';
+import fs from 'fs-extra';
+
+describe('mongo-schema-visit', () => {
+  describe('mongoSchemaVisit()', () => {
+    interface TestInput {
+      readonly schema: MongoJsonSchemaTypeObject;
+    }
+
+    const exampleMapping: ExampleMappingFn<TestInput> = (te) => {
+      return {
+        description: te.dir,
+        input: {
+          schema: textToJson<MongoJsonSchemaTypeObject>(te.files['input.json']),
+        },
+        expected: te.files['result.txt'],
+      };
+    };
+
+    const VISITOR: MongoJsonSchemaVisitor<string[]> = (
+      schema: MongoJsonSchemaAnyType,
+      propertyContext: MongoJsonSchemaPropertyContext | undefined,
+      parameter: string[] | undefined
+    ) => {
+      const propertyContextValues: readonly string[] = propertyContext
+        ? [
+            propertyContext.propertyName,
+            propertyContext.propertyIndex.toString(),
+            propertyContext.totalNumPropertiesInObject.toString(),
+          ]
+        : [];
+      const schemaString: string = [
+        schema.bsonType,
+        ...propertyContextValues,
+      ].join(', ');
+      parameter?.push(schemaString);
+    };
+
+    const EXAMPLES = getFileSystemTestExamples<TestInput>(
+      path.join(__dirname, 'test-assets'),
+      exampleMapping
+    );
+
+    EXAMPLES.forEach((example) => {
+      it(
+        example.description,
+        createFileSystemExampleTest(
+          example,
+          () => {
+            const result: string[] = [];
+            mongoSchemaVisit(example.input.schema, VISITOR, result);
+            // fs.writeFileSync(
+            //   path.join(__dirname, 'test-assets', 'result2.txt'),
+            //   result.join('\n')
+            // );
+            return result;
+          },
+          (result: string[]) => {
+            return result.join('\n') + '\n';
+          }
+        )
+      );
+    });
+  });
+});
