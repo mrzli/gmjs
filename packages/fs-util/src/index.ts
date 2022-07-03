e147cf605dda73c5283c4d22dbb3b6aaf9c690b8
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-03 16:34:09 +0200

    small update

--- packages/fs-util/src/index.ts
+++ packages/fs-util/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/file-system/file-system';
 export * from './lib/file-system/types';
+export * from './lib/file-system/util';
 export * from './lib/find';
 export * from './lib/find-types';
 export * from './lib/read-aggregate';
