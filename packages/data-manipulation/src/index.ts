527bb2c251bb02474cbb9d65dbb1146c47c9a190
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-03 21:23:47 +0200

    remove seed-db code generator

--- packages/data-manipulation/src/index.ts
+++ packages/data-manipulation/src/index.ts
@@ -3,11 +3,11 @@ export * from './lib/code/add-mongo-database-to-backend/add-mongo-database-to-ba
 export * from './lib/data-model/to-schema/data-model-to-schema';
 export * from './lib/schema/to-backend-app-code/schema-to-backend-app-code';
 export * from './lib/schema/to-backend-app-code/schema-to-backend-app-code-input';
+export * from './lib/schema/to-cli-app-code/schema-to-cli-app-code';
+export * from './lib/schema/to-cli-app-code/schema-to-cli-app-code-input';
 export * from './lib/schema/to-json-data/schema-to-json-data';
 export * from './lib/schema/to-postman-collection/schema-to-postman-collection';
 export * from './lib/schema/to-postman-collection/schema-to-postman-collection-input';
-export * from './lib/schema/to-seed-db-code/schema-to-seed-db-code';
-export * from './lib/schema/to-seed-db-code/schema-to-seed-db-code-input';
 export * from './lib/schema/to-shared-library-code/schema-to-shared-library-code';
 export * from './lib/schema/to-shared-library-code/schema-to-shared-library-code-input';
 export * from './lib/schema/to-types/schema-to-types';
