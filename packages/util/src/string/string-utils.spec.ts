81a8d2fb585e63fafe5d75fc7b8224664d51b5a2
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-14 21:29:20 +0200

    implement string trim functions

--- packages/util/src/string/string-utils.spec.ts
+++ packages/util/src/string/string-utils.spec.ts
@@ -1,4 +1,4 @@
-import { capitalize } from './string-utils';
+import { capitalize, trim, trimEnd, trimStart } from './string-utils';
 
 describe('string-utils', () => {
   describe('capitalize()', () => {
@@ -46,6 +46,241 @@ describe('string-utils', () => {
       it(JSON.stringify(example), () => {
         const actual = capitalize(example.input);
         expect(actual).toEqual(example.expected);
+      });
+    });
+  });
+
+  describe('trim', () => {
+    interface Example {
+      readonly input: {
+        readonly value: string;
+        readonly chars: string;
+      };
+      readonly expected: unknown;
+    }
+
+    describe('trimStart()', () => {
+      const EXAMPLES: readonly Example[] = [
+        {
+          input: {
+            value: '',
+            chars: '',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: '',
+            chars: 'a',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: ' b ',
+            chars: 'a',
+          },
+          expected: ' b ',
+        },
+        {
+          input: {
+            value: ' a ',
+            chars: 'a',
+          },
+          expected: ' a ',
+        },
+        {
+          input: {
+            value: ' a',
+            chars: 'a',
+          },
+          expected: ' a',
+        },
+        {
+          input: {
+            value: 'a ',
+            chars: 'a',
+          },
+          expected: ' ',
+        },
+        {
+          input: {
+            value: 'aabbaa',
+            chars: 'a',
+          },
+          expected: 'bbaa',
+        },
+        {
+          input: {
+            value: 'AAbbAA',
+            chars: 'a',
+          },
+          expected: 'AAbbAA',
+        },
+        {
+          input: {
+            value: 'aabbccbbaa',
+            chars: 'ab',
+          },
+          expected: 'ccbbaa',
+        },
+      ];
+
+      EXAMPLES.forEach((example) => {
+        it(JSON.stringify(example), () => {
+          const actual = trimStart(example.input.value, example.input.chars);
+          expect(actual).toEqual(example.expected);
+        });
+      });
+    });
+
+    describe('trimEnd()', () => {
+      const EXAMPLES: readonly Example[] = [
+        {
+          input: {
+            value: '',
+            chars: '',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: '',
+            chars: 'a',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: ' b ',
+            chars: 'a',
+          },
+          expected: ' b ',
+        },
+        {
+          input: {
+            value: ' a ',
+            chars: 'a',
+          },
+          expected: ' a ',
+        },
+        {
+          input: {
+            value: ' a',
+            chars: 'a',
+          },
+          expected: ' ',
+        },
+        {
+          input: {
+            value: 'a ',
+            chars: 'a',
+          },
+          expected: 'a ',
+        },
+        {
+          input: {
+            value: 'aabbaa',
+            chars: 'a',
+          },
+          expected: 'aabb',
+        },
+        {
+          input: {
+            value: 'AAbbAA',
+            chars: 'a',
+          },
+          expected: 'AAbbAA',
+        },
+        {
+          input: {
+            value: 'aabbccbbaa',
+            chars: 'ab',
+          },
+          expected: 'aabbcc',
+        },
+      ];
+
+      EXAMPLES.forEach((example) => {
+        it(JSON.stringify(example), () => {
+          const actual = trimEnd(example.input.value, example.input.chars);
+          expect(actual).toEqual(example.expected);
+        });
+      });
+    });
+
+    describe('trim()', () => {
+      const EXAMPLES: readonly Example[] = [
+        {
+          input: {
+            value: '',
+            chars: '',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: '',
+            chars: 'a',
+          },
+          expected: '',
+        },
+        {
+          input: {
+            value: ' b ',
+            chars: 'a',
+          },
+          expected: ' b ',
+        },
+        {
+          input: {
+            value: ' a ',
+            chars: 'a',
+          },
+          expected: ' a ',
+        },
+        {
+          input: {
+            value: ' a',
+            chars: 'a',
+          },
+          expected: ' ',
+        },
+        {
+          input: {
+            value: 'a ',
+            chars: 'a',
+          },
+          expected: ' ',
+        },
+        {
+          input: {
+            value: 'aabbaa',
+            chars: 'a',
+          },
+          expected: 'bb',
+        },
+        {
+          input: {
+            value: 'AAbbAA',
+            chars: 'a',
+          },
+          expected: 'AAbbAA',
+        },
+        {
+          input: {
+            value: 'aabbccbbaa',
+            chars: 'ab',
+          },
+          expected: 'cc',
+        },
+      ];
+
+      EXAMPLES.forEach((example) => {
+        it(JSON.stringify(example), () => {
+          const actual = trim(example.input.value, example.input.chars);
+          expect(actual).toEqual(example.expected);
+        });
       });
     });
   });
--- packages/util/src/string/string-utils.ts
+++ packages/util/src/string/string-utils.ts
@@ -5,3 +5,16 @@ export function capitalize(value: string): string {
 
   return value.slice(0, 1).toUpperCase() + value.slice(1).toLowerCase();
 }
+
+export function trimStart(value: string, chars: string): string {
+  return value.replace(new RegExp(`^[${chars}]+`), '');
+}
+
+export function trimEnd(value: string, chars: string): string {
+  return value.replace(new RegExp(`[${chars}]+$`), '');
+}
+
+export function trim(value: string, chars: string): string {
+  const startTrimmed = trimStart(value, chars);
+  return trimEnd(startTrimmed, chars);
+}
