33b18f9d5eb34204d29e9dce87cfe59570b87f04
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-17 20:44:14 +0200

    small updates

--- packages/data-manipulation/src/lib/schema/shared/constants.ts
+++ packages/data-manipulation/src/lib/schema/shared/constants.ts
@@ -6,6 +6,8 @@ export const MODULE_NAME_GMJS_UTIL = toGmjsModuleName('util');
 export const MODULE_NAME_GMJS_MONGO_UTIL = toGmjsModuleName('mongo-util');
 export const MODULE_NAME_GMJS_NEST_UTIL = toGmjsModuleName('nest-util');
 export const MODULE_NAME_GMJS_LIB_UTIL = toGmjsModuleName('lib-util');
+export const MODULE_NAME_GMJS_BROWSER_UTIL = toGmjsModuleName('browser-util');
+export const MODULE_NAME_GMJS_REACT_UTIL = toGmjsModuleName('react-util');
 
 function toGmjsModuleName(projectName: string): string {
   return `@${GMJS_NPM_SCOPE}/${projectName}`;
