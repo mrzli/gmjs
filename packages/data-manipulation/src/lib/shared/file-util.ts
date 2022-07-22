b0eb8df6cbddcf2ebd9fc6c8a69d89a17cb5dc35
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-22 09:55:36 +0200

    add function for generating files from a template in a folder

--- packages/data-manipulation/src/lib/shared/file-util.ts
+++ packages/data-manipulation/src/lib/shared/file-util.ts
@@ -1,9 +1,34 @@
 import { AnyObject } from '@gmjs/util';
 import ejs from 'ejs';
+import {
+  findFilesDeepSync,
+  PathContentPair,
+  readTextSync,
+} from '@gmjs/fs-util';
+import path from 'path';
 
 export function processTemplateFile(
   content: string,
   substitutions?: AnyObject
 ): string {
   return substitutions ? ejs.render(content, substitutions) : content;
+}
+
+export function generateFiles(
+  filesDir: string,
+  substitutions?: AnyObject
+): readonly PathContentPair[] {
+  const relativeFilePaths = getRelativeFilePaths(filesDir);
+  return relativeFilePaths.map((p) => ({
+    path: p,
+    content: processTemplateFile(
+      readTextSync(path.join(filesDir, p)),
+      substitutions
+    ),
+  }));
+}
+
+function getRelativeFilePaths(dir: string): readonly string[] {
+  const results = findFilesDeepSync(dir);
+  return results.map((r) => path.relative(dir, r.fullPath));
 }
