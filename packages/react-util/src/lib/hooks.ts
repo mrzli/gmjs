6593ea6ca9bc48de16c6221373a645adda5ad302
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-27 20:48:25 +0200

    add some rxjs hooks

--- packages/react-util/src/lib/hooks.ts
+++ packages/react-util/src/lib/hooks.ts
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from 'react';
+import { Observable, Subject, Unsubscribable } from 'rxjs';
 
 // https://blog.logrocket.com/how-to-get-previous-props-state-with-react-hooks/
 export function usePrevious<T>(value: T): T | undefined {
@@ -7,4 +8,54 @@ export function usePrevious<T>(value: T): T | undefined {
     ref.current = value;
   }, [value]);
   return ref.current;
+}
+
+export type SubjectPipeline<TInputValue, TOutputValue> = (
+  source$: Observable<TInputValue>
+) => Observable<TOutputValue>;
+export type MonoTypeSubjectPipeline<TValue> = SubjectPipeline<TValue, TValue>;
+export type ObservableSubscribe<TValue> = (value: TValue) => {
+  readonly unsubscribe: Unsubscribable;
+};
+
+export type SubjectOrUndefined<T> = Subject<T> | undefined;
+export type MutableRefSubject<T> = React.MutableRefObject<
+  SubjectOrUndefined<T>
+>;
+
+export function useSubjectRef<TInputValue, TOutputValue>(
+  pipeline: SubjectPipeline<TInputValue, TOutputValue>,
+  callback: (value: TOutputValue) => void
+): MutableRefSubject<TInputValue> {
+  const subjectRef: MutableRefSubject<TInputValue> =
+    useRef<SubjectOrUndefined<TInputValue>>(undefined);
+
+  useEffect(() => {
+    const subject$ = new Subject<TInputValue>();
+    const subscription = pipeline(subject$).subscribe(callback);
+    subjectRef.current = subject$;
+    return () => {
+      subscription.unsubscribe();
+      subjectRef.current = undefined;
+    };
+  }, [pipeline, callback]);
+
+  return subjectRef;
+}
+
+export const useMonoTypeSubjectRef: <TValue>(
+  pipeline: MonoTypeSubjectPipeline<TValue>,
+  callback: (value: TValue) => void
+) => MutableRefSubject<TValue> = useSubjectRef;
+
+export function useObservable<TValue>(
+  observable$: Observable<TValue>,
+  callback: (value: TValue) => void
+): void {
+  useEffect(() => {
+    const subscription = observable$.subscribe(callback);
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [observable$, callback]);
 }
