d312ec7a38d18a4380340a55243046eacb2a0b84
Goran Mržljak <goran.mrzljak@gmail.com>
2022-07-29 23:05:58 +0200

    publish

--- packages/mongo-util/src/index.ts
+++ packages/mongo-util/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/util';
 export * from './lib/data-converters';
 export * from './lib/execute-mongo';
 export * from './lib/mongo-operations';
+export * from './lib/mongo-database-service';
 export * from './lib/mongo-json-schema/mongo-json-schema';
 export * from './lib/mongo-json-schema/mongo-bson-type';
 export * from './lib/db-operations/create-db';
