f2fa84e098670934d66ae8291d59aa9f2b5fc777
Goran Mržljak <goran.mrzljak@gmail.com>
2022-08-14 00:34:55 +0200

    small fix

--- packages/mongo-util/src/index.ts
+++ packages/mongo-util/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/data-converters';
 export * from './lib/db-operations/execute-mongo';
 export * from './lib/db-operations/mongo-operations';
 export * from './lib/mongo-database-service';
+export * from './lib/mongo-database-id-generator';
 export * from './lib/mongo-json-schema/mongo-json-schema';
 export * from './lib/mongo-json-schema/mongo-bson-type';
 export * from './lib/db-operations/create-db';
