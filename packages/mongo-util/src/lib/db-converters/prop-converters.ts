f0795996d6b3d5acad9e60b732e1806ad5121225
Goran Mržljak <goran.mrzljak@gmail.com>
2022-08-17 12:25:38 +0200

    update property converters

--- packages/mongo-util/src/lib/db-converters/prop-converters.ts
+++ packages/mongo-util/src/lib/db-converters/prop-converters.ts
@@ -1,6 +1,7 @@
 import { transformIfExists } from '@gmjs/util';
 import { Decimal128, Double, Long, ObjectId } from 'mongodb';
 
+// db-to-app
 export function propIdDbToApp(value: ObjectId): string {
   return value.toString();
 }
@@ -11,6 +12,26 @@ export function propIdOptionalDbToApp(
   return transformIfExists(value, propIdDbToApp, undefined);
 }
 
+export function propStringDbToApp(value: string): string {
+  return value;
+}
+
+export function propStringOptionalDbToApp(
+  value: string | undefined
+): string | undefined {
+  return transformIfExists(value, propStringDbToApp, undefined);
+}
+
+export function propIntegerDbToApp(value: number): number {
+  return value;
+}
+
+export function propIntegerOptionalDbToApp(
+  value: number | undefined
+): number | undefined {
+  return transformIfExists(value, propIntegerDbToApp, undefined);
+}
+
 export function propLongDbToApp(value: Long): number {
   return value.toInt();
 }
@@ -19,6 +40,16 @@ export function propLongOptionalDbToApp(
   value: Long | undefined
 ): number | undefined {
   return transformIfExists(value, propLongDbToApp, undefined);
+}
+
+export function propFloatDbToApp(value: number): number {
+  return value;
+}
+
+export function propFloatOptionalDbToApp(
+  value: number | undefined
+): number | undefined {
+  return transformIfExists(value, propFloatDbToApp, undefined);
 }
 
 export function propDoubleDbToApp(value: Double): number {
@@ -41,6 +72,16 @@ export function propDecimalOptionalDbToApp(
   return transformIfExists(value, propDecimalDbToApp, undefined);
 }
 
+export function propBooleanDbToApp(value: boolean): boolean {
+  return value;
+}
+
+export function propBooleanOptionalDbToApp(
+  value: boolean | undefined
+): boolean | undefined {
+  return transformIfExists(value, propBooleanDbToApp, undefined);
+}
+
 export function propDateDbToApp(value: Date): string {
   return value.toISOString();
 }
@@ -50,7 +91,9 @@ export function propDateOptionalDbToApp(
 ): string | undefined {
   return transformIfExists(value, propDateDbToApp, undefined);
 }
+// end db-to-app
 
+// app-to-db
 export function propIdAppToDb(value: string): ObjectId {
   return new ObjectId(value);
 }
@@ -61,6 +104,26 @@ export function propIdOptionalAppToDb(
   return transformIfExists(value, propIdAppToDb, undefined);
 }
 
+export function propStringAppToDb(value: string): string {
+  return value;
+}
+
+export function propStringOptionalAppToDb(
+  value: string | undefined
+): string | undefined {
+  return transformIfExists(value, propStringAppToDb, undefined);
+}
+
+export function propIntegerAppToDb(value: number): number {
+  return value;
+}
+
+export function propIntegerOptionalAppToDb(
+  value: number | undefined
+): number | undefined {
+  return transformIfExists(value, propIntegerAppToDb, undefined);
+}
+
 export function propLongAppToDb(value: number): Long {
   return new Long(value);
 }
@@ -69,6 +132,16 @@ export function propLongOptionalAppToDb(
   value: number | undefined
 ): Long | undefined {
   return transformIfExists(value, propLongAppToDb, undefined);
+}
+
+export function propFloatAppToDb(value: number): number {
+  return value;
+}
+
+export function propFloatOptionalAppToDb(
+  value: number | undefined
+): number | undefined {
+  return transformIfExists(value, propFloatAppToDb, undefined);
 }
 
 export function propDoubleAppToDb(value: number): Double {
@@ -83,6 +156,16 @@ export function propDoubleOptionalAppToDb(
 
 export function propDecimalAppToDb(value: string): Decimal128 {
   return new Decimal128(value);
+}
+
+export function propBooleanAppToDb(value: boolean): boolean | undefined {
+  return value;
+}
+
+export function propBooleanOptionalAppToDb(
+  value: boolean | undefined
+): boolean | undefined {
+  return transformIfExists(value, propBooleanAppToDb, undefined);
 }
 
 export function propDecimalOptionalAppToDb(
@@ -100,3 +183,4 @@ export function propDateOptionalAppToDb(
 ): Date | undefined {
   return transformIfExists(value, propDateAppToDb, undefined);
 }
+// end app-to-db
--- packages/postgres-util/src/lib/db-converters/prop-converters.ts
+++ packages/postgres-util/src/lib/db-converters/prop-converters.ts
@@ -1,8 +1,101 @@
 import { transformIfExists } from '@gmjs/util';
 import { NULL, quoteValue } from './util';
 
-export function propIdAppToDb(value: string): string {
+// db-to-app
+export function propIdDbToApp(value: string): string {
   return value;
+}
+
+export function propIdOptionalDbToApp(
+  value: string | null
+): string | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propIdDbToApp);
+}
+
+export function propStringDbToApp(value: string): string {
+  return value;
+}
+
+export function propStringOptionalDbToApp(
+  value: string | null
+): string | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propStringDbToApp);
+}
+
+export function propIntegerDbToApp(value: number): number {
+  return value;
+}
+
+export function propIntegerOptionalDbToApp(
+  value: number | null
+): number | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propIntegerDbToApp);
+}
+
+export function propLongDbToApp(value: string): number {
+  return Number.parseInt(value);
+}
+
+export function propLongOptionalDbToApp(
+  value: string | null
+): number | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propLongDbToApp);
+}
+
+export function propFloatDbToApp(value: number): number {
+  return value;
+}
+
+export function propFloatOptionalDbToApp(
+  value: number | null
+): number | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propFloatDbToApp);
+}
+
+export function propDoubleDbToApp(value: number): number {
+  return value;
+}
+
+export function propDoubleOptionalDbToApp(
+  value: number | null
+): number | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propDoubleDbToApp);
+}
+
+export function propDecimalDbToApp(value: string): string {
+  return value;
+}
+
+export function propDecimalOptionalDbToApp(
+  value: string | null
+): string | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propDecimalDbToApp);
+}
+
+export function propBooleanDbToApp(value: boolean): boolean {
+  return value;
+}
+
+export function propBooleanOptionalDbToApp(
+  value: boolean | null
+): boolean | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propBooleanDbToApp);
+}
+
+export function propDateDbToApp(value: Date): string {
+  return value.toISOString();
+}
+
+export function propDateOptionalDbToApp(
+  value: Date | null
+): string | undefined {
+  return transformIfExistsFromDbOrUndefined(value, propDateDbToApp);
+}
+// end db-to-app
+
+// app-to-db
+export function propIdAppToDb(value: string): string {
+  return quoteValue(value);
 }
 
 export function propIdOptionalAppToDb(value: string | undefined): string {
@@ -71,6 +164,14 @@ export function propDateAppToDb(value: string): string {
 
 export function propDateOptionalAppToDb(value: string | undefined): string {
   return transformIfExistsOrDbNull(value, propDateAppToDb);
+}
+// end app-to-db
+
+function transformIfExistsFromDbOrUndefined<T, U>(
+  value: T | null,
+  transformer: (input: T) => U
+): U | undefined {
+  return transformIfExists(value, transformer, null) ?? undefined;
 }
 
 function transformIfExistsOrDbNull<T>(
