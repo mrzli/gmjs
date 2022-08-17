60da08e68a32469355eb9533228792936bc6a845
Goran Mržljak <goran.mrzljak@gmail.com>
2022-08-17 16:55:19 +0200

    refactor model converters

--- packages/postgres-util/src/lib/db-converters/model-converters.ts
+++ packages/postgres-util/src/lib/db-converters/model-converters.ts
@@ -1,19 +1,36 @@
 export function pgModelToInsert<TAppModel>(
   item: TAppModel,
   tableName: string,
-  columnNames: string,
-  getValues: (item: TAppModel) => string
+  columnNames: readonly string[],
+  getValues: (item: TAppModel) => readonly string[]
 ): string {
-  const values = getValues(item);
-  return `INSERT INTO ${tableName} (${columnNames}) VALUES ${values}`;
+  const columnNamesString = columnNames.join(', ');
+  const valuesString = getValues(item).join(', ');
+  return `INSERT INTO ${tableName} (${columnNamesString}) VALUES (${valuesString})`;
+}
+
+export function pgModelToUpdate<TAppModel>(
+  item: TAppModel,
+  tableName: string,
+  columnNames: readonly string[],
+  getValues: (item: TAppModel) => readonly string[],
+  idColumnName: string,
+  idColumnValue: string
+): string {
+  const columnNamesString = columnNames.join(', ');
+  const valuesString = getValues(item).join(', ');
+  return `UPDATE ${tableName} SET (${columnNamesString}) = (${valuesString}) WHERE ${idColumnName} = ${idColumnValue}`;
 }
 
 export function pgModelsToInsert<TAppModel>(
   items: readonly TAppModel[],
   tableName: string,
-  columnNames: string,
-  getValues: (item: TAppModel) => string
+  columnNames: readonly string[],
+  getValues: (item: TAppModel) => readonly string[]
 ): string {
-  const values = items.map((item) => `  (${getValues(item)})`).join(',\n');
-  return `INSERT INTO ${tableName} (${columnNames}) VALUES\n${values}`;
+  const columnNamesString = columnNames.join(', ');
+  const valuesString = items
+    .map((item) => `  (${getValues(item).join(', ')})`)
+    .join(',\n');
+  return `INSERT INTO ${tableName} (${columnNamesString}) VALUES\n${valuesString}`;
 }
