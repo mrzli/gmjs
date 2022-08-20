82ab8af77ded33efc86ae5f58a18ca6d94fa6c6b
Goran Mržljak <goran.mrzljak@gmail.com>
2022-08-20 23:46:05 +0200

    remove mongo-database module from nest-util
    It is no longer used in the new generator.

--- packages/nest-util/src/index.ts
+++ packages/nest-util/src/index.ts
@@ -1,5 +1,2 @@
 export * from './lib/nest-util';
 export * from './lib/database/database-service-wrapper';
-export * from './lib/mongo-database/mongo-database.module';
-export * from './lib/mongo-database/mongo-database.service';
-export * from './lib/mongo-database/mongo-database-config-options';
--- packages/nest-util/src/lib/mongo-database/mongo-database-config-options.ts
+++
@@ -1,5 +0,0 @@
-export interface MongoDatabaseConfigOptions {
-  readonly host: string;
-  readonly port: number;
-  readonly dbName: string;
-}
