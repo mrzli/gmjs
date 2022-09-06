7eea4fc618533810157dfa6749d51ceb9243dea0
Goran Mržljak <goran.mrzljak@gmail.com>
2022-09-06 15:04:55 +0200

    implement new transformer functions

--- packages/util/src/flow/transformers.spec.ts
+++ packages/util/src/flow/transformers.spec.ts
@@ -1,6 +1,6 @@
 import { Fn1 } from "../types/function";
 import { transformPipe } from "./function-pipe";
-import { filter, map } from './transformers';
+import { filter, flatten, map } from './transformers';
 
 describe('transformers', () => {
   describe('map()', () => {
@@ -29,6 +29,55 @@ describe('transformers', () => {
     EXAMPLES.forEach((example) => {
       it(JSON.stringify(example), () => {
         const actual = [...transformPipe(map(MAPPER))(example.input)];
+        expect(actual).toEqual(example.expected);
+      });
+    });
+  });
+
+  describe('flatten()', () => {
+    interface Example {
+      readonly input: readonly (readonly number[])[];
+      readonly expected: readonly number[];
+    }
+  
+    const EXAMPLES: readonly Example[] = [
+      {
+        input: [],
+        expected: [],
+      },
+      {
+        input: [[]],
+        expected: [],
+      },
+      {
+        input: [[], []],
+        expected: [],
+      },
+      {
+        input: [[1]],
+        expected: [1],
+      },
+      {
+        input: [[1], [2]],
+        expected: [1, 2],
+      },
+      {
+        input: [[1, 2]],
+        expected: [1, 2],
+      },
+      {
+        input: [[1, 2], [3, 4]],
+        expected: [1, 2, 3, 4],
+      },
+      {
+        input: [[1, 2], []],
+        expected: [1, 2],
+      },
+    ];
+  
+    EXAMPLES.forEach((example) => {
+      it(JSON.stringify(example), () => {
+        const actual = [...transformPipe(flatten())(example.input)];
         expect(actual).toEqual(example.expected);
       });
     });
--- packages/util/src/flow/transformers.ts
+++ packages/util/src/flow/transformers.ts
@@ -1,6 +1,8 @@
-import { Fn1 } from "../types/function";
+import { Fn1 } from '../types/function';
 
-export function map<T, U>(mapper: (item: T) => U): Fn1<Iterable<T>, Iterable<U>> {
+export function map<T, U>(
+  mapper: (item: T) => U
+): Fn1<Iterable<T>, Iterable<U>> {
   return function* (iterable: Iterable<T>): Iterable<U> {
     for (const item of iterable) {
       yield mapper(item);
@@ -8,7 +10,19 @@ export function map<T, U>(mapper: (item: T) => U): Fn1<Iterable<T>, Iterable<U>>
   };
 }
 
-export function filter<T>(predicate: (item: T) => boolean): Fn1<Iterable<T>, Iterable<T>> {
+export function flatten<T>(): Fn1<Iterable<Iterable<T>>, Iterable<T>> {
+  return function* (iterable: Iterable<Iterable<T>>): Iterable<T> {
+    for (const item of iterable) {
+      for (const innerItem of item) {
+        yield innerItem;
+      }
+    }
+  };
+}
+
+export function filter<T>(
+  predicate: (item: T) => boolean
+): Fn1<Iterable<T>, Iterable<T>> {
   return function* (iterable: Iterable<T>): Iterable<T> {
     for (const item of iterable) {
       if (predicate(item)) {
@@ -16,4 +30,10 @@ export function filter<T>(predicate: (item: T) => boolean): Fn1<Iterable<T>, Ite
       }
     }
   };
+}
+
+export function toArray<T>(): Fn1<Iterable<T>, readonly T[]> {
+  return (iterable: Iterable<T>) => {
+    return [...iterable];
+  }
 }
