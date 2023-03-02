37efbec40d3e1c889c382570473d937d88d4f7c8
Goran Mržljak <goran.mrzljak@gmail.com>
2023-03-02 20:08:46 +0100

    add clampNumber() function

--- packages/util/src/number/number-utils.ts
+++ packages/util/src/number/number-utils.ts
@@ -41,3 +41,7 @@ export function padNonNegativeIntWithZeroes(
 export function getRandomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function clampNumber(value: number, min: number, max: number): number {
+  return Math.min(Math.max(value, min), max)
+}
